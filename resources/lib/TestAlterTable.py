666bcc7cb203ed26c2e0f0fed6bc99b3fe989eea
Malte Löpmann <maloep@users.noreply.github.com>
2010-07-23 18:54:09 +0000

    - added ALTER DATABASE script

--- resources/lib/TestAlterTable.py
+++ resources/lib/TestAlterTable.py
@@ -24,6 +24,8 @@ class TestAlterTable(unittest.TestCase):
 		self.gdb.dropTables()		
 	
 	
+	"""
+	
 	def test_V04toV05(self):
 		#create Table V0.4
 		self.gdb.executeSQLScript(os.path.join(self.databasedir, 'SQL_CREATE_TESTDB_V0.4.txt'))
@@ -124,140 +126,15 @@ class TestAlterTable(unittest.TestCase):
 		self.assertEqual(fileTypes[5][3], 'game')
 		self.assertEqual(fileTypes[6][1], 'screenshottitle')				
 		self.assertEqual(fileTypes[7][1], 'cartridge')		
-		self.assertEqual(fileTypes[8][1], 'gameplay')
-		
-		
+		self.assertEqual(fileTypes[8][1], 'gameplay')	
 	"""
-	def test_V03toV05(self):
-		#create Table V0.3
-		self.gdb.executeSQLScript(os.path.join(self.databasedir, 'SQL_CREATE_V0.3.txt'))
-		#insert data
-		self.gdb.executeSQLScript(os.path.join(self.databasedir, 'SQL_INSERT_TEST_DATA_V0.3.txt'))
-
-		#convert V0.3 to V0.5
+	
+	def test_V05toV06(self):
+		#create Table V0.5
+		self.gdb.executeSQLScript(os.path.join(self.databasedir, 'SQL_CREATE_TESTDB_V0.5.txt'))
+		
+		#convert V0.5 to V0.6
 		self.gdb.checkDBStructure()
-		
-		#Test
-		rcbSettingRows = RCBSetting(self.gdb).getAll()
-		self.assertTrue(rcbSettingRows != None)
-		self.assertTrue(len(rcbSettingRows) == 1)
-			
-		rcbSetting = rcbSettingRows[0]
-		self.assertEqual(rcbSetting[10], 'V0.5')
-		self.assertEqual(rcbSetting[11], 'True')
-		self.assertEqual(rcbSetting[12], 'True')
-		self.assertEqual(rcbSetting[13], 'True')
-		self.assertEqual(rcbSetting[14], 'True')
-		self.assertEqual(rcbSetting[15], 'True')
-		self.assertEqual(rcbSetting[16], 'True')
-		
-		
-		consoleRows = Console(self.gdb).getAll()
-		self.assertTrue(consoleRows != None)
-		self.assertTrue(len(consoleRows) == 2)
-		
-		amiga = consoleRows[0]
-		self.assertEqual(amiga[1], 'Amiga')		
-		self.assertEqual(amiga[2], 'The Amiga 500 is also known as the A500 (or its code name Rock Lobster).')
-		self.assertEqual(amiga[3], 'E:\Emulatoren\data\consoleImages\uae.png')
-		
-		superNintendo = consoleRows[1]
-		self.assertEqual(superNintendo[1], 'Super Nintendo')
-		self.assertEqual(superNintendo[2], 'The Super Nintendo (SNES) is a 16-bit video game console released in 1990.')
-		self.assertEqual(superNintendo[3], 'E:\Emulatoren\data\consoleImages\zsnes.png')
-		
-		romCollectionRows = RomCollection(self.gdb).getAll()
-		self.assertTrue(romCollectionRows != None)
-		self.assertTrue(len(romCollectionRows) == 2)
-		
-		collV1 = romCollectionRows[0]
-		self.assertEqual(collV1[1], 'Collection V1')
-		console = Console(self.gdb).getObjectById(collV1[2])
-		self.assertEqual(console[1], 'Amiga')
-		self.assertEqual(collV1[3], 'winuae.exe {-%I% "%ROM%"}')
-		self.assertEqual(collV1[4], 'True')
-		self.assertEqual(collV1[5], 'True')
-		self.assertEqual(collV1[6], 'DescriptionParser.xml')
-		self.assertEqual(collV1[7], 'True')
-		self.assertEqual(collV1[8], 'False')
-		self.assertEqual(collV1[9], 'True')
-		self.assertEqual(collV1[10], '_Disk')
-		self.assertEqual(collV1[11], 'Text')
-		self.assertEqual(collV1[12], 'True')
-		self.assertEqual(collV1[13], 'True')
-				
-		
-		collV2 = romCollectionRows[1]
-		self.assertEqual(collV2[1], 'Collection V2')
-		console = Console(self.gdb).getObjectById(collV2[2])
-		self.assertEqual(console[1], 'Super Nintendo')
-		self.assertEqual(collV2[3], 'zsnes.exe -m "%ROM%"')	
-		self.assertEqual(collV2[4], 'True')
-		self.assertEqual(collV2[5], 'True')
-		self.assertEqual(collV2[6], 'DescriptionParser.xml')
-		self.assertEqual(collV2[7], 'True')
-		self.assertEqual(collV2[8], 'False')
-		self.assertEqual(collV2[9], 'True')
-		self.assertEqual(collV2[10], '_Disk')
-		self.assertEqual(collV2[11], 'Text')
-		self.assertEqual(collV2[12], 'True')
-		self.assertEqual(collV2[13], 'True')
-		
-		
-		fileTypes = FileType(self.gdb).getAll()
-		self.assertEqual(fileTypes[0][1], 'rcb_rom')
-		self.assertEqual(fileTypes[1][1], 'rcb_manual')
-		self.assertEqual(fileTypes[2][1], 'rcb_description')
-		self.assertEqual(fileTypes[3][1], 'rcb_configuration')		
-		self.assertEqual(fileTypes[4][1], 'cover')
-		self.assertEqual(fileTypes[5][1], 'screenshotingame')
-		self.assertEqual(fileTypes[6][1], 'screenshottitle')				
-		self.assertEqual(fileTypes[7][1], 'cartridge')		
-		self.assertEqual(fileTypes[8][1], 'gameplay')
-		
-		
-		
-		
-		paths = Path(self.gdb).getAll()
-		self.assertTrue(paths != None)
-		self.assertTrue(len(paths) == 11)
-		
-		self.assertEqual(paths[0][1], 'E:\Emulatoren\data\Amiga\Collection V1\\roms\*.adf')		
-		self.pathTest(paths[1][1], 'E:\Emulatoren\data\Amiga\Collection V1\synopsis\synopsis.txt', paths[1][2], 'rcb_description', paths[1][3], 'Collection V1')
-		self.pathTest(paths[2][1], 'E:\Emulatoren\data\Amiga\Collection V1\screens\%GAME%.jpg', paths[2][2], 'screenshotingame', paths[2][3], 'Collection V1')
-		self.pathTest(paths[3][1], 'E:\Emulatoren\data\Amiga\Collection V1\screens\%GAME%.gif', paths[3][2], 'screenshotingame', paths[3][3], 'Collection V1')
-		self.pathTest(paths[4][1], 'E:\Emulatoren\data\Amiga\Collection V1\screens\%GAME%.png', paths[4][2], 'screenshotingame', paths[4][3], 'Collection V1')
-		self.pathTest(paths[5][1], 'E:\Emulatoren\data\Amiga\Collection V1\cover\%GAME%.jpg', paths[5][2], 'cover', paths[5][3], 'Collection V1')
-		self.pathTest(paths[6][1], 'E:\Emulatoren\data\Amiga\Collection V1\ingameVideo\%GAME%.wmv', paths[6][2], 'ingamevideo', paths[6][3], 'Collection V1')
-		self.pathTest(paths[7][1], 'E:\Emulatoren\data\Amiga\Collection V1\manuals\%GAME%.txt', paths[7][2], 'rcb_manual', paths[7][3], 'Collection V1')
-		self.pathTest(paths[8][1], 'E:\Emulatoren\data\Amiga\Collection V1\\trailer\%GAME%.wmv', paths[8][2], 'trailer', paths[8][3], 'Collection V1')
-		self.pathTest(paths[9][1], 'E:\Emulatoren\data\Amiga\Collection V2\\roms\*.smc', paths[9][2], 'rcb_rom', paths[9][3], 'Collection V2')
-		self.pathTest(paths[10][1], 'E:\Emulatoren\data\Amiga\Collection V2\screens\*.png', paths[10][2], 'screenshotingame', paths[10][3], 'Collection V2')
-		
-		
-		gameRows = Game(self.gdb).getAllOrdered()	
-		self.assertTrue(gameRows != None)
-		self.assertTrue(len(gameRows) == 6)
-				
-		self.gameTest(gameRows[0], 'Airborne Ranger', 'In this action/simulation game by Microprose the player takes the role of an U.S. Army Airborne ranger.', 
-			'',  '', 1, 2, 2, 4, 3, '4', '5', 15, 'www', 'USA', 'Disk', '1st Person', 'Joystick', 0, 0,
-			1, 1, 1, 0, 0, 0)				
-		self.gameTest(gameRows[1], 'Dogfight', 'Dogfight is a two-player game with roots in the same primordial soup as Ataris Combat and other basic dogfighting games.', 	
-			'',  '', 1, 2, 2, 1, 4, '2', '4', 10, 'www', 'USA', 'Disk', '1st Person', 'Joystick', 0, 0,
-			3, 1, 1, 0, 0, 0)				
-		self.gameTest(gameRows[2], 'Football Glory', 'From Croatia came this overhead view football game resembling Sensible Soccer.', 
-			'',  '', 1, 4, 4, 2, 6, '2', '2', 7, 'www', 'USA', 'Disk', 'Top', 'Joystick', 0, 0,
-			3, 1, 1, 0, 0, 0)
-		self.gameTest(gameRows[3], 'Formula One Grand Prix', 'F1 is an Official Formula One Racing Game.', 
-			'',  '', 1, 5, 5, 1, 5, '2', '7', 32, 'www', 'USA', 'Disk', 'Top', 'Joystick', 0, 0,
-			4, 1, 1, 0, 0, 0)
-		self.gameTest(gameRows[4], 'Hanse - Die Expedition', 'Hanse makes you a trader.', 
-			'',  '', 1, 6, 6, 5, 6, '2', '2', 7, 'www', 'USA', 'Disk', 'Top', 'Joystick', 0, 0,
-			3, 1, 1, 0, 0, 0)
-		self.gameTest(gameRows[5], 'Legends of Zelda', 'This installment in the Zelda series was my favorite.', 
-			'', '', 2, 6, 6, 3, 6, '4', '10', 35, 'www', 'USA', 'Cartridge', 'Top', 'Gamepad', 0, 0,
-			1, 1, 1, 0, 0, 0)	
-	"""
 		
 		
 	def gameTest(self, game, name, descStart, gameCmd, alternateGameCmd, romCollectionId, publisherId, developerId, reviewerId,
