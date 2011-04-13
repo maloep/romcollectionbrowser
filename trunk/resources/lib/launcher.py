2993226cc9175a39bf7ec484d0aa03e9355fc98b
maloep <maloep@09d56a4c-1c0e-11df-9657-6b7988cda077>
2011-04-13 16:20:20 +0000

    - extracted methods

--- trunk/resources/lib/launcher.py
+++ trunk/resources/lib/launcher.py
@@ -7,80 +7,82 @@ import time, zipfile
 
 
 def launchEmu(gdb, gui, gameId, config, settings):
-		Logutil.log("Begin launcher.launchEmu", util.LOG_LEVEL_INFO)
+	Logutil.log("Begin launcher.launchEmu", util.LOG_LEVEL_INFO)
+	
+	gameRow = Game(gdb).getObjectById(gameId)
+	if(gameRow == None):
+		Logutil.log("Game with id %s could not be found in database" %gameId, util.LOG_LEVEL_ERROR)
+		return
 		
-		gameRow = Game(gdb).getObjectById(gameId)
-		if(gameRow == None):
-			Logutil.log("Game with id %s could not be found in database" %gameId, util.LOG_LEVEL_ERROR)
-			return
-			
-		romCollection = None
-		try:
-			romCollection = config.romCollections[str(gameRow[util.GAME_romCollectionId])]
-		except:
-			Logutil.log('Cannot get rom collection with id: ' +str(gameRow[util.GAME_romCollectionId]), util.LOG_LEVEL_ERROR)
-			
-		gui.writeMsg("Launch Game " + gameRow[util.ROW_NAME])
+	romCollection = None
+	try:
+		romCollection = config.romCollections[str(gameRow[util.GAME_romCollectionId])]
+	except:
+		Logutil.log('Cannot get rom collection with id: ' +str(gameRow[util.GAME_romCollectionId]), util.LOG_LEVEL_ERROR)
+		gui.writeMsg("Error launching game!")
+		return
 		
-		cmd = ""
-		
-		#get environment OS
-		env = ( os.environ.get( "OS", "win32" ), "win32", )[ os.environ.get( "OS", "win32" ) == "xbox" ]	
-				
-		filenameRows = File(gdb).getRomsByGameId(gameRow[util.ROW_ID])		
-		
-		escapeCmd = settings.getSetting(util.SETTING_RCB_ESCAPECOMMAND).upper() == 'TRUE'
-		cmd = buildCmd(filenameRows, romCollection, gameRow, escapeCmd)
+	gui.writeMsg("Launch Game " + gameRow[util.ROW_NAME])
+	
+	cmd = ""
+	
+	#get environment OS
+	env = ( os.environ.get( "OS", "win32" ), "win32", )[ os.environ.get( "OS", "win32" ) == "xbox" ]	
 			
-		if (romCollection.useEmuSolo):
-			
-			#try to create autoexec.py
-			writeAutoexec(gdb)
+	filenameRows = File(gdb).getRomsByGameId(gameRow[util.ROW_ID])		
+	
+	escapeCmd = settings.getSetting(util.SETTING_RCB_ESCAPECOMMAND).upper() == 'TRUE'
+	cmd = buildCmd(filenameRows, romCollection, gameRow, escapeCmd)
+		
+	if (romCollection.useEmuSolo):
+		
+		#try to create autoexec.py
+		writeAutoexec(gdb)
 
-			# Remember selection
-			gui.saveViewState(False)
-			
-			#invoke batch file that kills xbmc before launching the emulator			
-			if(env == "win32"):
-				#There is a problem with quotes passed as argument to windows command shell. This only works with "call"
-				cmd = 'call \"' +os.path.join(util.RCBHOME, 'applaunch.bat') +'\" ' +cmd						
-			else:
-				cmd = os.path.join(re.escape(util.RCBHOME), 'applaunch.sh ') +cmd
+		# Remember selection
+		gui.saveViewState(False)
+		
+		#invoke batch file that kills xbmc before launching the emulator			
+		if(env == "win32"):
+			#There is a problem with quotes passed as argument to windows command shell. This only works with "call"
+			cmd = 'call \"' +os.path.join(util.RCBHOME, 'applaunch.bat') +'\" ' +cmd						
 		else:
-			#use call to support paths with whitespaces
-			if(env == "win32" and not (os.environ.get( "OS", "xbox" ) == "xbox")):
-				cmd = 'call ' +cmd
+			cmd = os.path.join(re.escape(util.RCBHOME), 'applaunch.sh ') +cmd
+	else:
+		#use call to support paths with whitespaces
+		if(env == "win32" and not (os.environ.get( "OS", "xbox" ) == "xbox")):
+			cmd = 'call ' +cmd
+	
+	#update LaunchCount
+	launchCount = gameRow[util.GAME_launchCount]
+	Game(gdb).update(('launchCount',), (launchCount +1,) , gameRow[util.ROW_ID])
+	gdb.commit()
+	
+	Logutil.log("cmd: " +cmd, util.LOG_LEVEL_INFO)			
+	
+	try:
+		if (os.environ.get( "OS", "xbox" ) == "xbox"):			
+			launchXbox(gui, gdb, cmd, romCollection, filenameRows)
+		else:
+			launchNonXbox(cmd, romCollection)
+	
+		gui.writeMsg("")
+					
+	except Exception, (exc):
+		Logutil.log("Error while launching emu: " +str(exc), util.LOG_LEVEL_ERROR)
+		gui.writeMsg("Error while launching emu: " +str(exc))
 		
-		#update LaunchCount
-		launchCount = gameRow[util.GAME_launchCount]
-		Game(gdb).update(('launchCount',), (launchCount +1,) , gameRow[util.ROW_ID])
-		gdb.commit()
-		
-		Logutil.log("cmd: " +cmd, util.LOG_LEVEL_INFO)			
-		
-		try:
-			if (os.environ.get( "OS", "xbox" ) == "xbox"):			
-				launchXbox(gui, gdb, cmd, romCollection, filenameRows)
-			else:
-				launchNonXbox(cmd, romCollection)
-		
-			gui.writeMsg("")
-						
-		except Exception, (exc):
-			Logutil.log("Error while launching emu: " +str(exc), util.LOG_LEVEL_ERROR)
-			gui.writeMsg("Error while launching emu: " +str(exc))
-			
-		try:
-			#delete temporary files (from zip or 7z extraction
-			tempDir = getTempDir()
-			files = os.listdir(tempDir)
-			for file in files:
-				os.remove(os.path.join(tempDir, file))
-		except Exception, (exc):
-			Logutil.log("Error deleting files after launch emu: " +str(exc), util.LOG_LEVEL_ERROR)
-			gui.writeMsg("Error deleting files after launch emu: " +str(exc))
-		
-		Logutil.log("End launcher.launchEmu", util.LOG_LEVEL_INFO)
+	try:
+		#delete temporary files (from zip or 7z extraction
+		tempDir = getTempDir()
+		files = os.listdir(tempDir)
+		for file in files:
+			os.remove(os.path.join(tempDir, file))
+	except Exception, (exc):
+		Logutil.log("Error deleting files after launch emu: " +str(exc), util.LOG_LEVEL_ERROR)
+		gui.writeMsg("Error deleting files after launch emu: " +str(exc))
+	
+	Logutil.log("End launcher.launchEmu", util.LOG_LEVEL_INFO)
 		
 
 
@@ -104,13 +106,7 @@ def buildCmd(filenameRows, romCollection, gameRow, escapeCmd):
 	
 	#params could be: {-%I% %ROM%}
 	#we have to repeat the part inside the brackets and replace the %I% with the current index
-	obIndex = emuParams.find('{')
-	cbIndex = emuParams.find('}')			
-	replString = ''
-	if obIndex > -1 and cbIndex > 1:
-		replString = emuParams[obIndex+1:cbIndex]
-	emuParams = emuParams.replace("{", "")
-	emuParams = emuParams.replace("}", "")
+	emuParams, partToRepeat = prepareMultiRomCommand(emuParams)
 	
 	#insert game specific command
 	gameCmd = ''
@@ -123,7 +119,6 @@ def buildCmd(filenameRows, romCollection, gameRow, escapeCmd):
 	for fileNameRow in filenameRows:
 		fileName = fileNameRow[0]			
 		rom = ""
-		roms = []
 		#we could have multiple rom Paths - search for the correct one
 		for romPath in romCollection.romPaths:
 			rom = os.path.join(romPath, fileName)
@@ -135,64 +130,8 @@ def buildCmd(filenameRows, romCollection, gameRow, escapeCmd):
 
 		# If it's a .7z file
 		filext = rom.split('.')[-1]
-		
-		
 		if filext in compressedExtensions and not romCollection.doNotExtractZipFiles:
-			Logutil.log('Treating file as a compressed archive', util.LOG_LEVEL_INFO)
-			compressed = True						
-		
-			names = getNames(filext, rom)
-			if(names == None):
-				Logutil.log('Error handling compressed file', util.LOG_LEVEL_WARNING)
-				return ""
-			
-			chosenROM = -1
-			
-			#check if we should handle multiple roms
-			match = False
-			if(romCollection.diskPrefix != ''):
-				match = re.search(romCollection.diskPrefix.lower(), str(names).lower())
-			
-			if '%I%' in emuParams and match:
-				Logutil.log("Loading %d archives" % len(names), util.LOG_LEVEL_INFO)
-				archives = getArchives(filext, rom, names)
-				if(archives == None):
-					Logutil.log('Error handling compressed file', util.LOG_LEVEL_WARNING)
-					return ""
-				for archive in archives:					
-					newPath = os.path.join(getTempDir(), archive[0])
-					fp = open(newPath, 'wb')
-					fp.write(archive[1])
-					fp.close()
-					roms.append(newPath)
-				
-			elif len(names) > 1:
-				Logutil.log("The Archive has %d files" % len(names), util.LOG_LEVEL_INFO)
-				chosenROM = xbmcgui.Dialog().select('Choose a ROM', names)
-			elif len(names) == 1:
-				Logutil.log("Archive only has one file inside; picking that one", util.LOG_LEVEL_INFO)
-				chosenROM = 0
-			else:
-				Logutil.log("Archive had no files inside!", util.LOG_LEVEL_ERROR)
-				return ""
-		
-			if chosenROM != -1:
-				# Extract the chosen file to %TMP%
-				newPath = os.path.join(getTempDir(), names[chosenROM])
-				
-				Logutil.log("Putting extracted file in %s" % newPath, util.LOG_LEVEL_INFO)
-				
-				data = getArchives(filext, rom, [names[chosenROM]])
-				if(data == None):
-					Logutil.log('Error handling compressed file', util.LOG_LEVEL_WARNING)
-					return ""
-				fo = open(str(newPath), 'wb')
-				fo.write(data[0][1])
-				fo.close()
-				
-				# Point file name to the newly extracted file and continue
-				# as usual
-				roms = [newPath]
+			roms = handleCompressedFile(filext, rom, romCollection, emuParams)
 		
 		if len(roms) == 0:
 			roms = [rom]
@@ -211,7 +150,7 @@ def buildCmd(filenameRows, romCollection, gameRow, escapeCmd):
 				else:
 					cmd = '\"' +emuCommandLine +'\" ' +emuParams.replace('%I%', str(fileindex))
 			else:
-				newrepl = replString
+				newrepl = partToRepeat
 				if (escapeCmd):
 					newrepl = replaceRomnameInParams(newrepl, re.escape(rom), romCollection)					
 					emuCommandLine = re.escape(emuCommandLine)					
@@ -222,6 +161,81 @@ def buildCmd(filenameRows, romCollection, gameRow, escapeCmd):
 			fileindex += 1
 	
 	return cmd
+
+
+def prepareMultiRomCommand(emuParams):
+	obIndex = emuParams.find('{')
+	cbIndex = emuParams.find('}')			
+	partToRepeat = ''
+	if obIndex > -1 and cbIndex > 1:
+		partToRepeat = emuParams[obIndex+1:cbIndex]
+	emuParams = emuParams.replace("{", "")
+	emuParams = emuParams.replace("}", "")
+	
+	return emuParams, partToRepeat
+
+
+def handleCompressedFile(filext, rom, romCollection, emuParams):
+	
+	roms = []
+	
+	Logutil.log('Treating file as a compressed archive', util.LOG_LEVEL_INFO)
+	compressed = True						
+
+	names = getNames(filext, rom)
+	if(names == None):
+		Logutil.log('Error handling compressed file', util.LOG_LEVEL_WARNING)
+		return []
+	
+	chosenROM = -1
+	
+	#check if we should handle multiple roms
+	match = False
+	if(romCollection.diskPrefix != ''):
+		match = re.search(romCollection.diskPrefix.lower(), str(names).lower())
+	
+	if '%I%' in emuParams and match:
+		Logutil.log("Loading %d archives" % len(names), util.LOG_LEVEL_INFO)
+		archives = getArchives(filext, rom, names)
+		if(archives == None):
+			Logutil.log('Error handling compressed file', util.LOG_LEVEL_WARNING)
+			return []
+		for archive in archives:					
+			newPath = os.path.join(getTempDir(), archive[0])
+			fp = open(newPath, 'wb')
+			fp.write(archive[1])
+			fp.close()
+			roms.append(newPath)
+		
+	elif len(names) > 1:
+		Logutil.log("The Archive has %d files" % len(names), util.LOG_LEVEL_INFO)
+		chosenROM = xbmcgui.Dialog().select('Choose a ROM', names)
+	elif len(names) == 1:
+		Logutil.log("Archive only has one file inside; picking that one", util.LOG_LEVEL_INFO)
+		chosenROM = 0
+	else:
+		Logutil.log("Archive had no files inside!", util.LOG_LEVEL_ERROR)
+		return []
+
+	if chosenROM != -1:
+		# Extract the chosen file to %TMP%
+		newPath = os.path.join(getTempDir(), names[chosenROM])
+		
+		Logutil.log("Putting extracted file in %s" % newPath, util.LOG_LEVEL_INFO)
+		
+		data = getArchives(filext, rom, [names[chosenROM]])
+		if(data == None):
+			Logutil.log('Error handling compressed file', util.LOG_LEVEL_WARNING)
+			return []
+		fo = open(str(newPath), 'wb')
+		fo.write(data[0][1])
+		fo.close()
+		
+		# Point file name to the newly extracted file and continue
+		# as usual
+		roms = [newPath]
+		
+	return roms
 
 
 def replaceRomnameInParams(emuParams, rom, romCollection):
