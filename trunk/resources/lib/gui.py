9484744482325017e6695d33d6f48ca96784ac87
maloep <maloep@09d56a4c-1c0e-11df-9657-6b7988cda077>
2011-12-06 07:02:25 +0000

    - don't write error message if control is not found

--- trunk/resources/lib/gui.py
+++ trunk/resources/lib/gui.py
@@ -2258,7 +2258,7 @@ class UIGameDB(xbmcgui.WindowXML):
 			#HACK there seems to be a problem with recognizing the scrollbar controls
 			if(controlId not in (CONTROL_SCROLLBARS)):
 				Logutil.log("Control with id: %s could not be found. Check WindowXML file. Error: %s" % (str(controlId), str(exc)), util.LOG_LEVEL_ERROR)
-				self.writeMsg("Control with id: %s could not be found. Check WindowXML file." % str(controlId))
+				#self.writeMsg("Control with id: %s could not be found. Check WindowXML file." % str(controlId))
 			return None
 		
 		return control
