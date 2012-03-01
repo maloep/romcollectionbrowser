b4d0369823401a6b6607cdb15c2d82a4d3cb4c4e
maloep <maloep@09d56a4c-1c0e-11df-9657-6b7988cda077>
2012-03-01 20:06:49 +0000

    - logging change

--- trunk/resources/lib/gamedatabase.py
+++ trunk/resources/lib/gamedatabase.py
@@ -253,7 +253,7 @@ class Game(DataBaseObject):
 	
 	deleteQuery = "DELETE FROM Game WHERE id = ?"
 	
-	def __init__(self, gdb):		
+	def __init__(self, gdb):
 		self.gdb = gdb
 		self.tableName = "Game"
 		
@@ -261,7 +261,7 @@ class Game(DataBaseObject):
 		args = (romCollectionId, genreId, yearId, publisherId, isFavorite)
 		filterQuery = self.filterQuery %likeStatement
 		util.Logutil.log('searching games with query: ' +filterQuery, util.LOG_LEVEL_DEBUG)
-		util.Logutil.log('searching games with args: romCollectionId = %s, genreId = %s, yearId = %s, publisherId = %s, isFavorite = %s, characterFilter = %s' %(str(romCollectionId), str(genreId), str(yearId), str(publisherId), str(isFavorite), likeStatement), util.LOG_LEVEL_INFO)
+		util.Logutil.log('searching games with args: romCollectionId = %s, genreId = %s, yearId = %s, publisherId = %s, isFavorite = %s, characterFilter = %s' %(str(romCollectionId), str(genreId), str(yearId), str(publisherId), str(isFavorite), likeStatement), util.LOG_LEVEL_DEBUG)
 		games = self.getObjectsByWildcardQuery(filterQuery, args)
 		newList = self.encodeUtf8(games)
 		return newList
