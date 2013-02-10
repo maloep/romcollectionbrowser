6e739c027b9e9f8b19a260a85eceeab15a62a8d8
Malte Löpmann <maloep@users.noreply.github.com>
2013-02-10 07:42:50 +0000

    - add some more "intelligence" to batch script

--- resources/scriptfiles/applaunch.bat
+++ resources/scriptfiles/applaunch.bat
@@ -1,20 +1,27 @@
 
+cd /d "%XBMC_PROFILE_USERDATA%"
+cd ..
+set XBMCLaunchCmd="%XBMC_HOME%\XBMC.exe"
+REM Check for portable mode
+echo %XBMC_PROFILE_USERDATA%|find /i "portable_data">nul
+if errorlevel 0 if not errorlevel 1 set XBMCLaunchCmd=%XBMCLaunchCmd% -p
 
-REM Is XBMC running?
-taskkill /f /IM XBMC.exe
+echo Stopping XBMC...
+echo.
+taskkill /f /IM xbmc.exe>nul 2>nul
 
-REM Wait for the kill
-REM sleep 
-
-REM Launch app
+echo Starting %*...
+echo.
 %*
 
 
-REM SOMETIMES xbmc starts too fast, and on some hardware if there is still a millisecond of sound being used, XBMC starts witout sound and some emulators say there is a problem with the sound hardware. If so, remove comment:
-REM sleep 1
+REM SOMETIMES xbmc starts too fast, and on some hardware if there is still a millisecond of sound being used, XBMC starts witout sound and some emulators say there is a problem with the sound hardware. If so, remove the REM of the next line:
+REM timeout 1
 
+REM Restart XBMC
+echo Restarting XBMC...
 
 REM Done? Restart XBMC
-"%PROGRAMFILES%\XBMC\XBMC.exe"
+%XBMCLaunchCmd%
 
 
