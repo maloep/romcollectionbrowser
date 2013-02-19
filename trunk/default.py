98c363783ef84a9c72074175a8ecc84a631808e4
maloep <maloep@09d56a4c-1c0e-11df-9657-6b7988cda077>
2013-02-19 07:02:17 +0000

    - changed copyright info

--- trunk/default.py
+++ trunk/default.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Malte Loepmann (maloep@googlemail.com)
+# Copyright (C) 2009-2013 Malte Loepmann (maloep@googlemail.com)
 #
 # This program is free software; you can redistribute it and/or modify it under the terms 
 # of the GNU General Public License as published by the Free Software Foundation; 
