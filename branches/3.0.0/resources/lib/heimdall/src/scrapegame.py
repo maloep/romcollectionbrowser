9a8137a3b8e4bde038639835b95788c9331c509c
maloep <maloep@09d56a4c-1c0e-11df-9657-6b7988cda077>
2013-07-16 05:50:38 +0000

    - giantbomb scraper stub

---
+++ branches/3.0.0/resources/lib/heimdall/src/scrapegame.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from resources.lib.heimdall.src.heimdall.core import Engine, Subject
+from resources.lib.heimdall.src.heimdall.predicates import *
+from resources.lib.heimdall.src.heimdall.threadpools import MainloopThreadPool
+
+from resources.lib.heimdall.src.games import thegamesdb
+from resources.lib.heimdall.src.games import giantbomb
+
+import json
+import time
+import urlparse
+import sys
+
+import logging
+logging.basicConfig()
+logging.getLogger("heimdall").setLevel(logging.DEBUG)
+
+def main(uri):
+    
+    pool = MainloopThreadPool()
+    engine = Engine(pool)
+    engine.registerModule(giantbomb.module)
+
+    def c(error, subject):
+        if error:
+            pass
+            #raise error
+        print subject
+        pool.quit()
+
+    metadata = dict()
+    metadata[dc.title] = 'Super Mario Kart'
+    metadata[edamontology.data_3106] = 'Super Nintendo (SNES)'
+    subject = Subject("", metadata)
+    subject.extendClass("item.game")
+
+    engine.get(subject, c)
+
+    try:
+        pool.join()
+    except KeyboardInterrupt:
+        pool.quit()
+
+if __name__ == "__main__":
+    main(sys.argv[1] if len(sys.argv) >= 2 else None)
