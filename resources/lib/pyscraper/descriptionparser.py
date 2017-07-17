68d183c09b7b6cbf1e73c5b7bf9d69934c2b4104
bruny <adb@iinet.net.au>
2017-07-17 22:38:16 +1000

    Add None check at start so we can unindent a level

--- resources/lib/pyscraper/descriptionparser.py
+++ resources/lib/pyscraper/descriptionparser.py
@@ -30,64 +30,68 @@ class DescriptionParser(object):
 		log.debug("replaceResultTokens: {0}".format(resultAsDict))
 		for key in resultAsDict.keys():
 			grammarElement = self.grammarNode.find(key)
-			if(grammarElement != None):
-				appendResultTo = grammarElement.attrib.get('appendResultTo')
-				appendResultWith = grammarElement.attrib.get('appendResultWith')
-				replaceKeyString = grammarElement.attrib.get('replaceInResultKey')
-				replaceValueString = grammarElement.attrib.get('replaceInResultValue')
-				dateFormat = grammarElement.attrib.get('dateFormat')
-				del grammarElement
 
-				#TODO: avoid multiple loops
-				if(appendResultTo != None or appendResultWith != None or dateFormat != None):
-					itemList = resultAsDict[key]
-					for i in range(0, len(itemList)):
-						try:
-							item = itemList[i]
-							newValue = item
+			if grammarElement is None:
+				log.warn("Could not find grammar element {0}".format(key))
+				continue
+
+			appendResultTo = grammarElement.attrib.get('appendResultTo')
+			appendResultWith = grammarElement.attrib.get('appendResultWith')
+			replaceKeyString = grammarElement.attrib.get('replaceInResultKey')
+			replaceValueString = grammarElement.attrib.get('replaceInResultValue')
+			dateFormat = grammarElement.attrib.get('dateFormat')
+			del grammarElement
+
+			#TODO: avoid multiple loops
+			if(appendResultTo != None or appendResultWith != None or dateFormat != None):
+				itemList = resultAsDict[key]
+				for i in range(0, len(itemList)):
+					try:
+						item = itemList[i]
+						newValue = item
+						del item
+						if(appendResultTo != None):
+							newValue = appendResultTo +newValue
+						if(appendResultWith != None):
+							newValue = newValue + appendResultWith
+						if(dateFormat != None):
+							if(dateFormat == 'epoch'):
+								try:
+									newValue = time.gmtime(int(newValue))
+								except:
+									print 'error converting timestamp: ' +str(newValue)
+							else:
+								newValue = time.strptime(newValue, dateFormat)
+						itemList[i] = newValue
+					except Exception, (exc):
+						print "Error while handling appendResultTo: " +str(exc)
+
+				resultAsDict[key] = itemList
+				del itemList
+
+			if(replaceKeyString != None and replaceValueString != None):
+				replaceKeys = replaceKeyString.split(',')
+				replaceValues = replaceValueString.split(',')
+
+				if(len(replaceKeys) != len(replaceValues)):
+					print "Configuration error: replaceKeys must be the same number as replaceValues"
+
+				itemList = resultAsDict[key]
+				for i in range(0, len(itemList)):
+					try:
+						item = itemList[i]
+
+						for j in range(len(replaceKeys)):
+							replaceKey = replaceKeys[j]
+							replaceValue = replaceValues[j]
+
+							newValue = item.replace(replaceKey, replaceValue)
 							del item
-							if(appendResultTo != None):
-								newValue = appendResultTo +newValue
-							if(appendResultWith != None):
-								newValue = newValue + appendResultWith
-							if(dateFormat != None):
-								if(dateFormat == 'epoch'):
-									try:
-										newValue = time.gmtime(int(newValue))
-									except:
-										print 'error converting timestamp: ' +str(newValue)
-								else:
-									newValue = time.strptime(newValue, dateFormat)
 							itemList[i] = newValue
-						except Exception, (exc):
-							print "Error while handling appendResultTo: " +str(exc)
+					except:
+						print "Error while handling appendResultTo"
 
-					resultAsDict[key] = itemList
-					del itemList
-
-				if(replaceKeyString != None and replaceValueString != None):
-					replaceKeys = replaceKeyString.split(',')
-					replaceValues = replaceValueString.split(',')
-
-					if(len(replaceKeys) != len(replaceValues)):
-						print "Configuration error: replaceKeys must be the same number as replaceValues"
-
-					itemList = resultAsDict[key]
-					for i in range(0, len(itemList)):
-						try:
-							item = itemList[i]
-
-							for j in range(len(replaceKeys)):
-								replaceKey = replaceKeys[j]
-								replaceValue = replaceValues[j]
-
-								newValue = item.replace(replaceKey, replaceValue)
-								del item
-								itemList[i] = newValue
-						except:
-							print "Error while handling appendResultTo"
-
-					resultAsDict[key] = itemList
-					del itemList
+				resultAsDict[key] = itemList
+				del itemList
 
 		return resultAsDict
