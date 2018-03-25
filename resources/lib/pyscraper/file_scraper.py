1940eb850dd0dcfa77f2cc14c428ffb56210a008
Malte Löpmann <maloep@googlemail.com>
2018-03-25 09:46:14 +0200

    whitespaces

--- resources/lib/pyscraper/file_scraper.py
+++ resources/lib/pyscraper/file_scraper.py
@@ -2,7 +2,7 @@ from scraper import AbstractScraper
 
 
 class FileScraper(AbstractScraper):
-	"""FileScraper is kept for inheritance purposes - we don't have any useful functions yet but may in the future
-	"""
-	def __init__(self):
-		pass
+    """FileScraper is kept for inheritance purposes - we don't have any useful functions yet but may in the future
+    """
+    def __init__(self):
+        pass
