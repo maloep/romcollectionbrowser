a773feabdc5a2d6b7fab1134f23579e26ef4c6cf
Malte Löpmann <maloep@googlemail.com>
2018-03-25 12:47:07 +0200

    update string ids in language files

--- resources/lib/temptests.py
+++ resources/lib/temptests.py
@@ -8,19 +8,31 @@ from config import *
 BASE_RESOURCE_PATH = os.path.join(os.getcwd())
 sys.path.append(os.path.join(BASE_RESOURCE_PATH, "pyscraper"))
 
-env = (os.environ.get("OS", "win32"), "win32",)[ os.environ.get("OS", "win32") == "xbox" ]
-if env == 'Windows_NT':
-	env = 'win32'
-sys.path.append(os.path.join(BASE_RESOURCE_PATH, "..", "platform_libraries", env))
-
 
 
 """
-title = 'Chuck Rock'
+path = '..\\language\\Swedish\\strings.xml'
 
-mobyurl = 'https://api.mobygames.com/v1/games?title=%s&platform=19&format=brief&api_key=FH9VxTkB6BGAEsF3qlnnxQ==' %title
+import io
 
-r = requests.get(mobyurl)
+content = ''
+with io.open(path, 'r', encoding="utf-8") as language_file:
+	content = language_file.read()
 
-print r.json()
+with io.open(path, 'w', encoding="utf-8") as language_file:
+	content = content.replace("5400", "3275")
+	content = content.replace("5500", "3280")
+	content = content.replace("5600", "3285")
+	content = content.replace("5700", "3290")
+	content = content.replace("350", "320")
+	content = content.replace("400", "321")
+	content = content.replace("401", "322")
+	content = content.replace("450", "323")
+	content = content.replace("500", "324")
+	content = content.replace("510", "325")
+	content = content.replace("520", "326")
+	content = content.replace("530", "327")
+	content = content.replace("30000", "32999")
+
+	language_file.write(content)
 """
